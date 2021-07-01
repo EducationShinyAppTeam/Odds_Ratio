60c54e979506a6af6121968dc5e34f6974cb3d2a
Shravani Samala <54723328+shravanisamala@users.noreply.github.com>
2021-07-01 16:09:07 -0400

    Updated updater

--- app.R
+++ app.R
@@ -105,7 +105,7 @@ ui <- list(
             br(),
             br(),
             br(),
-            div(class = "updated", "Last Update: 5/14/2021 by NJH.")
+            div(class = "updated", "Last Update: 7/1/2021 by SJS.")
           )
         ),
         #### Set up the Prerequisites Page ----
